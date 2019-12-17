dd8b0cc008951c7cfe64ac49f844c0ca9206f58f
Wallace Frota <frotadev@gmail.com>
2019-12-17 10:31:30 -0300

    adicionado função que joga os dados da tabela cursos na div criado para exibi-los

--- js/loja.js
+++ js/loja.js
@@ -49,3 +49,45 @@ function cursos_insert_db(tx) {
     $("#tela_inserir").hide();
     $("#tela_carrinho").hide();
 }
+
+// buscando valores da bd
+function cursos_view() {
+    db.transaction(cursos_view_db, errorDB, successDB);
+}
+
+// monta a matriz com os registros da tabela cursos
+function cursos_view_db(tx) {
+    tx.executeSql('SELECT * FROM Cursos', [], cursos_view_data, errorDB);
+}
+
+// mostra os registros da tabela cursos
+function cursos_view_data(tx, results) {
+    $("#cursos_listagem").empty();
+
+    var len = results.rows.length;
+
+    for(var i = 0; i < len; i++) {
+        if (len === "" || len === 0) {
+            $("#alert").append(
+                "<div class='alert alert-warning' role='alert'>" +
+                     "NENHUM CURSO DISPONÍVEL NO MOMENTO!" +
+                 "</div>"
+            );
+        } else {
+            // lista os cursos na div cursos_listagem
+            $("#cursos_listagem").append(
+            "<div class='col-sm-6 col-md-4 mb-3 no-padding'>" +
+            "<div class='card'>" +
+            "<div class='card-body'>" +
+            "<h4 class='card-title'>" + results.rows.item(i).nome + "</h4>" +
+            "<p class='card-text'>" + results.rows.item(i).desc + "</p>" +
+            "<p class='card-text'>" + "Vagas Disponíveis: " + results.rows.item(i).qtd + "</p>" +
+            "<p class='card-text font-weight-bold'>" + "Valor R$ " + results.rows.item(i).preco + ",00" + "</p>" +
+            "<input type='button' class='btn btn-primary form-control' data-toggle='modal' data-target='#tela_carrinho' value='Comprar' onclick='curso_abrir_carrinho(" + results.rows.item(i).id + ")'></input>" +
+            "</div>" +
+            "</div>" +
+            "</div>"
+            );
+        }
+    }
+}
