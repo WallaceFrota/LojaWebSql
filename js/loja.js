a71384faad726f7645e2021bb21b01f0cd386f5e
Wallace Frota <frotadev@gmail.com>
2019-12-17 10:25:51 -0300

    adicionado função que insere dados digitados na tabela de cursos

--- js/loja.js
+++ js/loja.js
@@ -28,3 +28,24 @@ function createDB(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS Cursos (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome VARCHAR(50), desc VARCHAR(50), qtd NUM(120), preco NUM(120))');
     tx.executeSql('CREATE TABLE IF NOT EXISTS Compras(id INTEGER PRIMARY KEY, nome VARCHAR(50), valor NUM(15), vagas NUM(15))');
 }
+// prepara para incluir registros na tabela cursos
+function cursos_insert() {
+    db.transaction(cursos_insert_db, errorDB, successDB);
+}
+
+// incluir registros na tabela cursos
+function cursos_insert_db(tx) {
+    var nome = $("#curso_name").val();
+    var desc = $("#curso_desc").val();
+    var qtd = $("#curso_qtd").val();
+    var preco = $("#curso_price").val();
+
+    tx.executeSql('INSERT INTO Cursos (nome, desc, qtd, preco) VALUES ("' + nome + '", "' + desc + '", "' + qtd + '", "' + preco + '")');
+
+    alert("CURSO CADASTRADO COM SUCESSO!")
+
+    // telas exibição
+    location.reload(true); // recarrega
+    $("#tela_inserir").hide();
+    $("#tela_carrinho").hide();
+}
